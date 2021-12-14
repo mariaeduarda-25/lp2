6d7783fce7a57f858d3d7688ac62b3afefa74c3b
Maria Eduarda <85760601+mariaeduarda-25@users.noreply.github.com>
2021-12-14 10:10:45 -0300

    Delete b3a2 directory

--- b3a2/atividade.py
+++
@@ -1,8 +0,0 @@
-import sqlalchemy as db
-
-
-engine = db.create_engine("mysql+pymysql://primeiro_20202017612:cefetMG20202017612@localhost/primeiro_20202017612")
-connection = engine.connect ()
-
-
-metadata = db.MetaData()
